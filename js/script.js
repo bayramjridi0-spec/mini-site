7476991883fecf978775e8c6bf9292875f807d70
bayramjridi0-spec <bayramjridi0@gmail.com>
2026-03-25 10:37:06 +0100

    feat: add basic HTML, CSS, JS structure with example script

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function() {
+    const titre = document.querySelector("h1");
+    titre.addEventListener("click", function() {
+        alert("Bravo ! Vous avez cliqué sur le titre 😎");
+    });
+});
